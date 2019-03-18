c223f8a31f8ae3fb29d30b72b01a04b878aea832
Thomas Murphy <thomasmurphy987@gmail.com>
2019-03-18 09:00:07 +0000

    Local hazelcast example working

--- src/main/java/com/tdm/example/poc/controller/HazelCastController.java
+++ src/main/java/com/tdm/example/poc/controller/HazelCastController.java
@@ -1,10 +1,38 @@
 package com.tdm.example.poc.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/poc/hzl")
 public class HazelCastController {
+
+    private final HazelcastInstance hazelcastInstance;
+
+    @Autowired
+    HazelCastController(HazelcastInstance hazelcastInstance){
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    @PostMapping("/write-data")
+    public String writeData(@RequestParam String key, @RequestParam String value){
+        Map<String,String> hazelcastMap = hazelcastInstance.getMap("my-map");
+        hazelcastMap.put(key, value);
+        return "Data has been stored in hazelcast.";
+    }
+
+    @GetMapping("/read-data")
+    public String readData(@RequestParam String key){
+        Map<String,String> hazelcastMap = hazelcastInstance.getMap("my-map");
+        return hazelcastMap.get(key);
+    }
+
+    @GetMapping("/get-all")
+    public Map<String,String> getAllData(){
+        return hazelcastInstance.getMap("my-map");
+    }
 
 }
